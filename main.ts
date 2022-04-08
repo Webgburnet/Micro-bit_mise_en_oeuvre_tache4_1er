4b86cf73f41f1c904b7e4319b5a4d8a95b0bf64b
Gregoire BURNET <44721323+Webgburnet@users.noreply.github.com>
2022-04-08 13:37:27 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+let strip = neopixel.create(DigitalPin.P0, 30, NeoPixelMode.RGB)
+strip.setBrightness(200)
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        for (let index = 0; index <= 19; index++) {
+            strip.setPixelColor(index, neopixel.rgb(255, 69, 0))
+            strip.show()
+            basic.pause(100)
+        }
+    }
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+})
